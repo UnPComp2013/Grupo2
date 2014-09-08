d3f6f5e3623cd086a24be72b73334d88ff2ca214
FranciscoOliveira <francisco-teless@hotmail.com>
2014-09-08 01:42:04 -0300

    Update Cadastro.sql

--- Cadastro.sql
+++ Cadastro.sql
@@ -1,1 +1,1 @@
-INSERT INTO Sistemas.dbo.tbl_rfaccount (id,password,accounttype,birthdate,Email) VALUES ((CONVERT(binary, ' teste10 ')), (CONVERT(binary, ' teste ')),1,'01/01/2011 01:00:00 AM','francisco_josebs@hotmail.com ')
+INSERT INTO Sistemas.dbo.tbl_conta(id,password,accounttype,birthdate,Email) VALUES ((CONVERT(binary, ' teste10 ')), (CONVERT(binary, ' teste ')),1,'01/01/2011 01:00:00 AM','francisco_josebs@hotmail.com ')
