83ca323fe6ecbb17baa93b02e2f89134c3fbceed
FranciscoOliveira <francisco-teless@hotmail.com>
2014-09-08 01:41:23 -0300

    Create Cadastro.sql

---
+++ Cadastro.sql
@@ -0,0 +1,1 @@
+INSERT INTO Sistemas.dbo.tbl_rfaccount (id,password,accounttype,birthdate,Email) VALUES ((CONVERT(binary, ' teste10 ')), (CONVERT(binary, ' teste ')),1,'01/01/2011 01:00:00 AM','francisco_josebs@hotmail.com ')
