1a4c8b45dda6a027be28744679b978ffd72dde1b
Tardelle <tardellesilva@gmail.com>
2014-09-09 14:56:25 -0300

    Create terca1.cpp

---
+++ terca1.cpp
@@ -0,0 +1,14 @@
+#include "terca1.h"
+#include "ui_terca1.h"
+
+Terca1::Terca1(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Terca1)
+{
+    ui->setupUi(this);
+}
+
+Terca1::~Terca1()
+{
+    delete ui;
+}
