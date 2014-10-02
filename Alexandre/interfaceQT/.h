e88002921f98bc0e930491ccdb85e1ead7316119
alexandreoliveira018 <alexandre.silva_16@hotmail.com>
2014-10-01 23:26:37 -0300

    Update .h

--- Alexandre/interfaceQT/.h
+++ Alexandre/interfaceQT/.h
@@ -1,1 +1,23 @@
-k
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
+
