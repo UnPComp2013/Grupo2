626506b38a4312e9b4e36e6a5a14611fc4bf593d
Tardelle <tardellesilva@gmail.com>
2014-10-15 14:13:11 -0300

    Update segunda1.cpp

--- segunda1.cpp
+++ segunda1.cpp
@@ -4,13 +4,7 @@
 Segunda1::Segunda1(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Segunda1)
-{
-    ui->setupUi(this);
-     for(int i = 0; i < 9; i++)
-    {
-        ui->listWidget->addItem(QString::number(i) + "frango");
-    }
-}
+
 
 Segunda1::~Segunda1()
 {
